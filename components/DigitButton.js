56faf7098a05b4a791b626378fece2cd023e4f58
Gautam Jain <astrogeek77@gmail.com>
2021-11-25 19:27:20 +0530

    initial commit

---
+++ components/DigitButton.js
@@ -0,0 +1,11 @@
+import { ACTIONS } from "../pages/index"
+
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  )
+}
